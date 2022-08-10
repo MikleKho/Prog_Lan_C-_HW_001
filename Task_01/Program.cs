5f8bcbe894635004ab30f190dcb2d3b472588b12
«MikleKho» <khomich.mikhail@gmail.com>
2022-08-10 17:11:23 +0300

    task_01 сделана

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Введите");
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadValue = Console.ReadLine();
+    int result = int.Parse(ReadValue);
+    return result;
+}
+int value_01 = Prompt("Введите первое число -> ");
+int value_02 = Prompt("Введите второе число -> ");
+if (value_01 > value_02)
+{
+    System.Console.Write($"Max = {value_01} Min = {value_02}");
+}
+else
+{
+    if (value_01 < value_02)
+    {
+        System.Console.Write($"Max = {value_02} Min = {value_01}");
+    }
+    else
+    {
+        System.Console.Write($"Numbers {value_02} and {value_01} are equal");
+    }
+}
