d5086daaa9e2f21818a62731b6179de3c8f216f3
«MikleKho» <khomich.mikhail@gmail.com>
2022-08-10 18:12:30 +0300

    task_04 initial commit

---
+++ Task_04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadValue = Console.ReadLine();
+    int result = int.Parse(ReadValue);
+    return result;
+}
+int value_01 = Prompt("Введите число -> ");
+
+if ((value_01 % 2) == 0)
+{
+    System.Console.Write("Вы ввели четное число");
+}
+else
+{
+    System.Console.Write("Вы ввели нечетное число");
+}
